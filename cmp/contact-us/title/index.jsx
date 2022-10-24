205321af726d9282a257bf5413f844cd2d680c89
Chandan Raj <chandanraaz91@gmail.com>
2022-10-24 12:31:53 +0530

    home-routing-added

--- cmp/contact-us/title/index.jsx
+++ cmp/contact-us/title/index.jsx
@@ -10,7 +10,7 @@ const Title =()=>{
         <div>
           <h2 className={`${Style.title}`}>Contact us</h2>
           <ul className={`${Style.ul_con}`}>
-            <li className={`${Style.li_con}`}><Link href="/home" className={`${Style.a_home}`}>HOME</Link></li>
+            <li className={`${Style.li_con}`}><Link href="/" className={`${Style.a_home}`}>HOME</Link></li>
             <li className={`${Style.li_con}`}><Link href="/contact-us" className={`${Style.a_contact}`}> / CONTACT US</Link></li>
           </ul>
         </div>
