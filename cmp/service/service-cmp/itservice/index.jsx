a00de6f48260850f3b495240aa8aa02016d11fc6
Chandan Raj <chandanraaz91@gmail.com>
2022-10-24 11:54:17 +0530

    arranged codes in systemetic way

--- cmp/service/service-cmp/itservice/index.jsx
+++ cmp/service/service-cmp/itservice/index.jsx
@@ -1,19 +1,18 @@
-import Style from './style.module.css';
+import Style from './itservice.module.css';
 
 const ItService = ()=>{
-    const design = (
+    return(
         <>
-        <div className={` ${Style.mainImage} d-flex align-items-center justify-content-center contaner-fluid mb-5`}>
-            <div className={`text-center`}>
-                <h1 className={`${Style.head_service}`}>IT Services</h1>
-                <div>
-                <span className={`${Style.home}`}>HOME</span>
-                <span className={`${Style.it_service}`}> / IT SERVICES</span>
-                </div>
-            </div>    
-        </div>
+            <div className={` ${Style.mainImage} d-flex align-items-center justify-content-center contaner-fluid mb-5`}>
+                <div className={`text-center`}>
+                    <h1 className={`${Style.head_service}`}>IT Services</h1>
+                    <div>
+                        <span className={`${Style.home}`}>HOME</span>
+                        <span className={`${Style.it_service}`}> / IT SERVICES</span>
+                    </div>
+                </div>    
+            </div>
         </>
     );
-    return design;
 }
 export default ItService;
